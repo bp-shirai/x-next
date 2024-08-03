cbf5364ba32af2e260d35db26def9184b7f8077f
bp.shirai <bp.shirai@gmail.com>
2024-08-03 17:59:23 +0900

    Update the News component

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,16 +10,25 @@ export const metadata = {
   description: "A clone of X website built with Next.js and Tailwind CSS",
 };
 
+export const dynamic = "force-dynamic";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <div className="flex justify-between max-w-6xl mx-auto">
-          <div>
+          <div className="hidden sm:inline border-r h-screen sticky top-0">
             <Sidebar />
           </div>
-          <div>{children}</div>
-          <div>
+          <div className="w-2xl flex-1">{children}</div>
+          <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+            <div className="sticky top-0 bg-white py-2">
+              <input
+                type="text"
+                placeholder="Search"
+                className="bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2"
+              ></input>
+            </div>
             <News />
           </div>
         </div>
